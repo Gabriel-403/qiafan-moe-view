6769b0066344b4aad90ed3cedefa2fb7145ce72f
chaosprogrammer <1499015923@qq.com>
2020-09-23 22:37:22 +0800

    9

--- src/pages/ManagePage/component/sidebar/index.jsx
+++ src/pages/ManagePage/component/sidebar/index.jsx
@@ -38,7 +38,7 @@ export default  class MangeSiderbar extends Component {
           <Menu.Item key="6">Option 6</Menu.Item>
           <SubMenu key="sub3" title="Submenu">
             <Menu.Item key="7">Option 7</Menu.Item>
-            <Menu.Item key="9">Option 8</Menu.Item>
+            <Menu.Item key="9">Option 9</Menu.Item>
           </SubMenu>
         </SubMenu>
         <SubMenu
