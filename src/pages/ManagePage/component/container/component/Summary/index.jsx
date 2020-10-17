10fa27e5b4b3917be52660ede484ec9991773a28
chaosprogrammer <1499015923@qq.com>
2020-10-17 22:44:13 +0800

    rechart

--- src/pages/ManagePage/component/container/component/Summary/index.jsx
+++ src/pages/ManagePage/component/container/component/Summary/index.jsx
@@ -10,7 +10,7 @@ const data = [
     name: '50+', uv: 2.63, pv: 4800, fill: '#d0ed57',
   },
   {
-    name: 'unknow', uv: 6.67, pv: 4800, fill: '#ffc658',
+    name: 'unknown', uv: 6.67, pv: 4800, fill: '#ffc658',
   },
 ];
 
