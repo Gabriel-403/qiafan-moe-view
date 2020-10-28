74b2c5eba22bbc0485fd50361cd284c3a83dd794
chaosprogrammer <1499015923@qq.com>
2020-10-28 22:43:32 +0800

    feat: update

--- src/pages/ManagePage/component/sidebar/index.jsx
+++ src/pages/ManagePage/component/sidebar/index.jsx
@@ -7,7 +7,7 @@ import './index.scss';
 
 const { SubMenu } = Menu;
 
-export default class MangeSiderbar extends Component {
+export default class MangeSidebar extends Component {
   render() {
     return (
       <div className="sidebar">
--- src/pages/ManagePage/index.jsx
+++ src/pages/ManagePage/index.jsx
@@ -3,10 +3,10 @@ import { Route } from "react-router-dom"
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux';
 import { onload } from '../../store/homePage/actions';
-import MangeSiderbar from "./component/Sidebar";
-import MangeHeader from "./component/Header"
-import Culture from "./component/container/component/Cultural"
-import Summary from "./component/container/component/Summary"
+import MangeSidebar from "./component/Sidebar";
+import MangeHeader from "./component/Header";
+import Culture from "./component/container/component/Cultural";
+import Summary from "./component/container/component/Summary";
 
 import './index.scss';
 import Stuff from './component/container/component/Stuff';
@@ -25,7 +25,7 @@ class MangerPage extends Component {
 			<section className="manager-container">
 				<MangeHeader></MangeHeader>
 				<section className="manager-body">
-					<MangeSiderbar>	</MangeSiderbar>
+					<MangeSidebar></MangeSidebar>
 					<main className="manager-content">
 						<Route exact path="/manage/cultural" component={Culture} />
 						<Route exact path="/manage/summary" component={Summary} />
