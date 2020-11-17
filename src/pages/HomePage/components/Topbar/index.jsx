509e57530e09f845a75a18dff78311b24d334c89
chaosprogrammer <1499015923@qq.com>
2020-11-18 01:18:39 +0800

    ci

--- src/pages/HomePage/components/Topbar/index.jsx
+++ src/pages/HomePage/components/Topbar/index.jsx
@@ -9,13 +9,15 @@ export default class Homepage extends React.Component {
 
 
   render() {
-    return (<div className="home-page"><header >
-        <img class="logo" src={imgURL} alt="image" />
-        <a class="active-item" >首页</a>
-        <a class="active-item2">专栏</a>
-        <a class="active-item2" href="/#/login">汉化组管理</a>
-      </header> </div>
-      
+    return (<div className="home-page">
+      <header>
+      <img class="logo" src={imgURL} alt="image" />
+      <a class="active-item" >首页</a>
+      <a class="active-item2">专栏</a>
+      <a class="active-item2" href="/#/login">汉化组管理</a>
+    </header> 
+    </div>
+
     )
   }
 }
